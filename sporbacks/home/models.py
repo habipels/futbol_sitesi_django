53b787c6979d0c96bc82703158cc1ce937087e2b
Habip ELİŞ <habibelis65@gmail.com>
2022-07-28 21:14:21 +0300

    #sosyal media ekleme degistime guncellemesi

--- sporbacks/home/models.py
+++ sporbacks/home/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class sosyal_media(models.Model):
+    isim = models.CharField(max_length=100)
+    links = models.CharField(max_length=300)
+    def __str__(self) :
+        return self.isim
