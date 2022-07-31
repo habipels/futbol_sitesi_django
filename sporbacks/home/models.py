1ad260e1adddc234e6a81da3af936a052b10d755
Habip ELİŞ <habibelis65@gmail.com>
2022-07-31 20:21:57 +0300

    #clubs models.py

--- sporbacks/home/models.py
+++ sporbacks/home/models.py
@@ -7,3 +7,8 @@ class sosyal_media(models.Model):
     links = models.CharField(max_length=300)
     def __str__(self) :
         return self.isim
+
+class clubs(models.Model):
+    takim_isim = models.CharField(max_length=200)
+    takim_isim_kisaltma = models.CharField(max_length=4)
+    takim_logo = models.ImageField(upload_to='takim_resim/',blank = True,null = True,verbose_name="Takım Resimi")
